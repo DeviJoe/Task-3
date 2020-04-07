813bdad7b8c504629777f781c1285c864de38135
Khakimov Lev <hakimov_l_n@sc.vsu.ru>
2020-04-08 01:23:04 +0300

    Реализация очереди Queue

---
+++ src/com/devijoe/project/Main.java
@@ -0,0 +1,17 @@
+package com.devijoe.project;
+
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+	// write your code here
+        Queue <Integer> q = new Queue<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.remove(2);
+        q.add(4);
+
+    }
+}
---
+++ src/com/devijoe/project/Queue.java
@@ -0,0 +1,128 @@
+package com.devijoe.project;
+
+public class Queue<T> {
+
+    /**
+     * Узел очереди
+     */
+    private class Node {
+
+        public transient T value;
+        public transient Node next;
+
+        public Node(T value, Node next) {
+            this.next = next;
+            this.value = value;
+        }
+
+        public Node() {
+            this(null, null);
+        }
+    }
+
+    // Первый элемент очереди
+    private transient Node head = null;
+    // Последний элемент очереди
+    private transient Node tail = null;
+    // Длина очереди
+    private transient int size = 0;
+
+    /**
+     * Получение длины очереди
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Подсмотреть и удалить верхушку очереди
+     * @return
+     * @throws Exception
+     */
+    public T get() throws Exception {
+        if (head == null) {
+            throw new Exception("Queue is EMPTY!");
+        }
+        T result = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return result;
+    }
+
+    /**
+     * Удаление верхушки
+     * @throws Exception
+     */
+    public void poll() throws Exception {
+        if (head == null) {
+            throw new Exception("EMPTY!");
+        }
+        T result = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+    }
+
+    /**
+     * Удаление верхушки
+     * @throws Exception
+     */
+    public void remove() throws Exception {
+        poll();
+    }
+
+    /**
+     * Удаление первого вхождения элемента в очередь
+     */
+    public void remove(T t) throws Exception {
+        if (head.value == t) {
+            remove();
+        } else {
+            Node x = head;
+            Node prev = null;
+            while (!(x.value == t)) {
+                prev = x;
+                x = x.next;
+            }
+            prev.next = x.next;
+        }
+    }
+
+    /**
+     * Добавление элемента
+     * @param t
+     */
+    public void add(T t) {
+        if (head == null) {
+            head = tail = new Node(t, null);
+        } else {
+            tail.next = new Node(t, null);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    /**
+     * Очистка очереди
+     */
+    public void clear() {
+        head.value = null;
+        head.next = null;
+        tail.value = null;
+        size = 0;
+    }
+
+    /**
+     * Показывает вершину очереди
+     */
+    public T peek() {
+        return head.value;
+    }
+
+}
